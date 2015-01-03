e489c6a21f33de7e931f53c1d4384e2a10f226c2
Dampe <isdampe@gmail.com>
2015-01-03 20:40:11 +1100

    Fixed directory

--- roll-static.sh
+++ roll-static.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/isdampe/Web-App-Min-2015.git
-mv WebAppMin2015/* ./
-rm WebAppMin2015 -Rf
+mv Web-App-Min-2015/* ./
+rm Web-App-Min-2015 -Rf
 chmod +x launch.sh install.sh
 ./install.sh
