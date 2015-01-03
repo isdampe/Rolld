7d0977dff77a9658d07504a004d53aa63e0e80ed
Dampe <isdampe@gmail.com>
2015-01-03 20:37:56 +1100

    New static web app repository

--- roll-static.sh
+++ roll-static.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git clone https://github.com/evasivesoftware/WebAppMin.git
-mv WebAppMin/* ./
-rm WebAppMin -Rf
-npm install
+git clone https://github.com/isdampe/Web-App-Min-2015.git
+mv WebAppMin2015/* ./
+rm WebAppMin2015 -Rf
+chmod +x launch.sh install.sh
+./install.sh
