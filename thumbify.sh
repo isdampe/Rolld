35007e5712166a30b9e1b45dcfd5aabfa3cca88e
Richard Denton <isdampe@gmail.com>
2016-03-29 08:54:13 +1100

    Adds more thumbifys

--- thumbify.sh
+++ thumbify.sh
@@ -1,3 +1,5 @@
-#/bin/sh
-mogrify -format jpg -quality 50 -resize 1500x *.jpg
-mogrify -gravity center -background white -extent 1500x1125 *.jpg
+#!/bin/sh
+mogrify -format png -resize 1500x *.png
+mogrify -gravity center -background transparent -extent 1500x1125 *.png
+mogrify -format jpg -quality 75 -background white -alpha remove *.png
+rm *.png
